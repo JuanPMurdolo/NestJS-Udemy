f1700cc8ac94be4a90ca5cedc5b12c680bf5f30e
Juan Murdolo <juanmurdolo@L-F760QJ03VG.local>
2024-02-01 12:35:14 -0300

    DTO

--- nestjs-task-management/src/tasks/tasks.controller.ts
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
+import { CreateTaskDTO } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,10 +13,8 @@ export class TasksController {
     }
 
     @Post()
-    createTask(@Body('title') title: string, @Body('description') description: string): Task{
-        console.log('title', title);
-        console.log('description', description);
-        return this.tasksService.createTask(title, description);
+    createTask(@Body() createTaskDTO: CreateTaskDTO): Task{
+        return this.tasksService.createTask(createTaskDTO);
     }
 
     @Get('/:id')
