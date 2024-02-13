25b0d2160552490a7e2e7ba5c2905f791d244381
Juan Murdolo <juanmurdolo@L-F760QJ03VG.local>
2024-02-13 10:20:33 -0300

    filtering

--- nestjs-task-management/src/tasks/dto/get-tasks-filter.dto.ts
+++ nestjs-task-management/src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsOptional, IsString, IsEnum } from "class-validator";
 import { TaskStatus } from "../task.model";
 
 export class GetTaskFilterDto {
+    @IsOptional()
+    @IsEnum(TaskStatus)
     status?: TaskStatus;
+
+    @IsOptional()
+    @IsString()
     search?: string;
 }
