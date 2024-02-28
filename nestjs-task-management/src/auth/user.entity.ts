be45c95ecfbb69acc3b02430a18a5be86122bce4
JuanPMurdolo <jpmurdolo@gmail.com>
2024-02-28 16:03:02 -0300

    User to task relation

--- nestjs-task-management/src/auth/user.entity.ts
+++ nestjs-task-management/src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Task } from "src/tasks/task.entity";
+import { Column, Entity, OneToMany, PrimaryColumn } from "typeorm";
 
 @Entity()
 export class User {
@@ -10,4 +11,7 @@ export class User {
 
     @Column()
     password: string;
+
+    @OneToMany(type => Task, task => task.user, { eager: true })
+    tasks: Task[];
 }
--- nestjs-task-management/src/tasks/task.entity.ts
+++ nestjs-task-management/src/tasks/task.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, ManyToMany, ManyToOne } from 'typeorm';
 import { TaskStatus } from './task-status.enum';
+import { User } from 'src/auth/user.entity';
 
 @Entity()
 export class Task {
@@ -10,8 +11,12 @@ export class Task {
   title: string;
 
   @Column()
+
   description: string;
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne(type => User, user => user.tasks, { eager: false })
+  user:User
 }
